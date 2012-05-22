494c56e196eeb8217d3f628c6367bbd9150aaab5
Patrick de Kok <patrick@cxiu.nl>
2012-05-22 23:33:41 +0200

    Started writing the layout of the GAViewer wrapper.

---
+++ PLViewer/plviewer/PLViewer.java
@@ -0,0 +1,14 @@
+package plviewer;
+
+public class PLViewer {
+  public static void main(String[] args) {
+    // 1. Read from configuration file where GAViewer is located.
+    // 2. Open GAViewer.
+    // 3. Wait for user to open GAViewer, open ports for communication.
+    // 4. Do REPL-like stuff:
+    //    1. Read user input
+    //    2. Evaluate user input
+    //    3. Print user input (and visualize in GAViewer)
+    //    4. Loop; go back to 4.1
+  }
+}
