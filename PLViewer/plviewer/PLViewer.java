2aab4556871efc0d4a37296f4b2d0d010ee475e8
Patrick de Kok <patrick@cxiu.nl>
2012-05-26 11:11:46 +0200

    More work on parsing arguments.

--- PLViewer/plviewer/PLViewer.java
+++ PLViewer/plviewer/PLViewer.java
@@ -2,11 +2,16 @@ package plviewer;
 
 import java.util.Arrays;
 import java.util.Properties;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.Writer;
 
 import java.io.FileNotFoundException;
@@ -37,84 +42,35 @@ public class PLViewer {
   public final static String DEFAULT_PROPERTIES = "default.properties";
   /**
    * Allowed no-argument command line options. Like {@code -r} in {@code rm -r /}.
+   * <p>
+   * Each element should have the following layout:
+   * {@code {flag1, ...}, {Properties key, Properties value Human readable comment}}
    */
-  public final static String[] flags = {};
+  public final static String[][][] flags = {
+    // {{flag1, ...}, {Properties key, Properties value, Human readable comment}}
+    {{"-h", "--help"}, {"DisplayHelp", "true", "This help text."}},
+  };
   /**
    * Allowed command line options with value. Like {@code --color} in {@code
    * ls --color=always}.
+   * <p>
+   * Each element should have the following layout:
+   * {@code {flag1, ...}, {Properties key, Human readable comment}}
    */
-  public final static String[][] arguments = {
-    {"-g", "--gaviewer"},
-    {"-f", "--properties"},
-    {"-p", "--port"},
+  public final static String[][][] arguments = {
+    // {{flag1, ...}, {Properties key, Human readable comment}}
+    {{"-g", "--gaviewer"}, {"GAViewer.custom", "The path to a user defined GAViewer."}},
+    {{"-f", "--properties"}, {"proppath", "The path to a custom properties file."}},
+    {{"-p", "--port"}, {"GAViewer.port", "Network port to use for communication with GAViewer."}},
     };
 
   public static void main(String[] args) {
-    // Check command line arguments
-    String custom_gaviewer = "";
-    String custom_properties = "";
-    String custom_port = "";
-    if (args.length > 0) {
-      for (int i = 0; i < args.length; i++) {
-        if (Arrays.binarySearch(arguments[0], args[i]) > -1) {
-          custom_gaviewer = args[++i];
-        }
-        else if (Arrays.binarySearch(arguments[1], args[i]) > -1) {
-          custom_properties = args[++i];
-        }
-        else if (Arrays.binarySearch(arguments[2], args[i]) > -1) {
-          custom_port = args[++i];
-        }
-        else {
-          String legalArguments = "";
-          for (String flag : flags) {
-            legalArguments += flag + "\n";
-          }
-          for (String[] arggroup : arguments) {
-            for (String arg : arggroup) {
-              legalArguments += arg + " ";
-            }
-            legalArguments += "\n";
-          }
-          new Inform("Illegal command line argument. Legal arguments:\n" + legalArguments, System.err);
-          System.exit(-1);
-        }
-      }
-    }
-
     // 1. Read from configuration file where GAViewer is located.
-    Properties configuration = new Properties(getDefaultConfiguration());
-    InputStream is;
-    try {
-      if (custom_properties.isEmpty()) {
-        is = PLViewer.class.getResourceAsStream("default.properties");
-      }
-      else {
-        is = new FileInputStream(custom_properties);
-      }
-      configuration.load(is);
-    }
-    catch (FileNotFoundException e) {
-      NullPointerException e2 = new NullPointerException(e.getMessage());
-      e2.initCause(e.getCause());
-      e2.setStackTrace(e.getStackTrace());
-      throw e2;
-    }
-    catch (NullPointerException e) {
-      new Inform("Configuration file not found. Generating default configuration file.", System.err);
-      generateDefaultConfigurationFile();
-    }
-    catch (IOException e) {
-      new Inform("Configuration file could not be loaded. The default configuration will be used.");
-    }
-
-    if (!custom_gaviewer.isEmpty()) {
-      configuration.setProperty("GAViewer.custom", custom_gaviewer);
-    }
-    if (!custom_port.isEmpty()) {
-      configuration.setProperty("GAViewer.port", custom_port);
-    }
-
+    Properties custom = processArgs(args);
+    Properties configuration = readPropertiesFile(custom.getProperty("proppath"));
+    custom.remove("proppath");
+    configuration.putAll(custom);
+    
     // 2. Open right GAViewer for OS.
     GAViewerClient gaviewer = new GAViewerClient(configuration);
 
@@ -123,14 +79,25 @@ public class PLViewer {
       new Inform("Obviously, something is going wrong when connecting to GAViewer. Check if it's running.", true);
     }
 
-    // 4. Do REPL-like stuff:
+    // 4. Do REPL-like stuff.
     //    1. Read user input
     //    2. Evaluate user input
     //    3. Print user input (and visualize in GAViewer)
     //    4. Loop; go back to 4.1
+    BufferedReader userReader = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter userWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    BufferedReader gavReader = new BufferedReader(new FilteredReader(gaviewer.getReader(), 
+        new ConformalToPluckerFilter()));
+    PrintWriter gavWriter = new PrintWriter(new FilteredWriter(gaviewer.getWriter(), 
+        new PluckerToConformalFilter()));
+
+    repl(userReader, userWriter, gavReader, gavWriter);
 
     // 5. Finish the process.
     quit(new Closeable[]{gaviewer});
+  }
+
+  public static void repl(BufferedReader userReader, PrintWriter userWriter, BufferedReader systemReader, PrintWriter systemWriter) {
   }
 
   /**
@@ -150,6 +117,112 @@ public class PLViewer {
       catch (IOException e) {
       }
     }
+  }
+
+  /**
+   * Parses the command line arguments, and put their values in a {@link
+   * java.util.Properties}.
+   *
+   * @param args The command line arguments that should be processed.
+   * @return A mapping representing the user defined settings.
+   */
+  protected static Properties processArgs(String[] args) {
+    Properties custom = new Properties();
+    boolean optionFound = false;
+    if (args.length > 0) {
+      for (int i = 0; i < args.length; i++) {
+        for (String[][] flaggroup : flags) {
+          if (Arrays.binarySearch(flaggroup[0], args[i]) > -1) {
+            custom.setProperty(flaggroup[-1][0], flaggroup[-1][1]);
+            optionFound = true;
+            break;
+          }
+        }
+        if (!optionFound) {
+          for (String[][] arggroup : arguments) {
+            if (Arrays.binarySearch(arggroup[0], args[i]) > -1) {
+              custom.setProperty(arggroup[-1][0], args[++i]);
+              optionFound = true;
+              break;
+            }
+          }
+        }
+        if (!optionFound) {
+          new Inform("Illegal command line argument: " + args[i] + ".", System.err);
+          displayArgs(System.err);
+          System.exit(-1);
+        }
+        optionFound = false;
+      }
+    }
+
+    if (custom.getProperty("DisplayHelp").equals("true")) {
+      displayArgs(System.out);
+      System.exit(-1);
+    }
+
+    return custom;
+  }
+
+  /**
+   * Display all valid command line arguments with explanation.
+   *
+   * @param os The stream to which this information is printed.
+   */
+  protected static void displayArgs(PrintStream os) {
+    String legalArguments = "";
+    for (String[][][] arggroups : new String[][][][]{flags, arguments}) {
+      for (String[][] arggroup : arggroups) {
+        for (String arg : arggroup[0]) {
+          legalArguments += " " + arg;
+        }
+        if (arggroup.length > 1) {
+          if (arggroup[-1].length > 1 && !arggroup[-1][-1].isEmpty()) {
+            legalArguments += " -- " + arggroup[-1][-1];
+          }
+        }
+        legalArguments += "\n";
+      }
+    }
+    new Inform("Possible command line arguments:\n" + legalArguments, os);
+  }
+
+  /**
+   * Read a .properties file and return the representing {@link
+   * java.util.Properties}.  If no file is given, the {@link
+   * #DEFAULT_PROPERTIES} file is read.
+   *
+   * @param filename The file that is read. If {@code filename} is the empty
+   * string, the {@link #DEFAULT_PROPERTIES} file is read.
+   * @return A map that represents the settings in {@code filename}.
+   */
+  protected static Properties readPropertiesFile(String filename) {
+    Properties configuration = new Properties(getDefaultConfiguration());
+    InputStream is;
+    try {
+      if (filename.isEmpty()) {
+        is = PLViewer.class.getResourceAsStream(DEFAULT_PROPERTIES);
+      }
+      else {
+        is = new FileInputStream(filename);
+      }
+      configuration.load(is);
+    }
+    catch (FileNotFoundException e) {
+      NullPointerException e2 = new NullPointerException(e.getMessage());
+      e2.initCause(e.getCause());
+      e2.setStackTrace(e.getStackTrace());
+      throw e2;
+    }
+    catch (NullPointerException e) {
+      new Inform("Configuration file not found. Generating default configuration file.", System.err);
+      generateDefaultConfigurationFile();
+    }
+    catch (IOException e) {
+      new Inform("Configuration file could not be loaded. The default configuration will be used.");
+    }
+
+    return configuration;
   }
 
   /**
